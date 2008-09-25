5273af19baad18433541a305e13eecc79411b206
jelaiw <jelaiw@users.noreply.github.com>
2008-09-25 15:31:00 +0000

    Added first pass at parser for the SNP file format of the "SNP_Map.txt" file that accompanies the genotyping results file from deCODE (for a custom Illumina SNP array). Added unit test and a small, representative SNP file for testing.

---
+++ src/java-tests/edu/uab/ssg/io/decode/TestSNPFileParser.java
@@ -0,0 +1,51 @@
+package edu.uab.ssg.io.decode;
+
+import junit.framework.TestCase;
+import junit.framework.Assert;
+import java.io.*;
+
+/**
+ * @author Jelai Wang
+ */
+
+public final class TestSNPFileParser extends TestCase {
+	public void testExampleFile() throws IOException {
+		InputStream in = getClass().getClassLoader().getResourceAsStream("edu/uab/ssg/io/decode/snps.txt");
+		SNPFileParser parser = new SNPFileParser();
+		TestHelper helper = new TestHelper();
+		parser.parse(in, helper);
+		Assert.assertEquals(19, helper.getNumberOfRecords());
+	}
+
+	private static final class TestHelper implements SNPFileParser.RecordListener {
+		private int numOfRecords = 0;
+
+		public void handle(SNPFileParser.SNPRecord record) {
+			// Spot check specific records.
+			if (numOfRecords == 0) { // First record.
+				Assert.assertEquals("AICDA-007875", record.getName());
+				Assert.assertEquals("12", record.getChromosome());
+				Assert.assertEquals(8650011, record.getPosition());
+				Assert.assertTrue(Double.compare(0.5980, record.getGenTrainScore()) == 0);
+				Assert.assertEquals("[T/C]", record.getSNP());
+				Assert.assertEquals("BOT", record.getILMNStrand());
+				Assert.assertEquals("TOP", record.getCustomerStrand());
+				Assert.assertEquals("0", record.getNormID());
+			}
+			else if (numOfRecords == 18) { // Last record.
+				Assert.assertEquals("CCL11-004869", record.getName());
+				Assert.assertEquals("17", record.getChromosome());
+				Assert.assertEquals(29639668, record.getPosition());
+				Assert.assertTrue(Double.compare(0.7965, record.getGenTrainScore()) == 0);
+				Assert.assertEquals("[A/C]", record.getSNP());
+				Assert.assertEquals("TOP", record.getILMNStrand());
+				Assert.assertEquals("TOP", record.getCustomerStrand());
+				Assert.assertEquals("0", record.getNormID());
+			}
+			// Increment total number of handled records.
+			numOfRecords++;
+		}
+
+		private int getNumberOfRecords() { return numOfRecords; }
+	}
+}
---
+++ src/java/edu/uab/ssg/io/decode/SNPFileParser.java
@@ -0,0 +1,79 @@
+package edu.uab.ssg.io.decode;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * @author Jelai Wang
+ */
+public final class SNPFileParser {
+	public void parse(InputStream in, RecordListener listener) throws IOException {
+		if (in == null)
+			throw new NullPointerException("in");
+		if (listener == null)
+			throw new NullPointerException("listener");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		String header = reader.readLine(); // Discard first row of column names.
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			listener.handle(new ParsedSNPRecord(line));
+		}
+		reader.close();
+	}
+
+	public interface RecordListener {
+		void handle(SNPRecord record);
+	}
+
+	public interface SNPRecord {
+		String getName();
+		String getChromosome();
+		int getPosition();
+		double getGenTrainScore();
+		String getSNP();
+		String getILMNStrand();
+		String getCustomerStrand();
+		String getNormID();
+	}
+
+	private class ParsedSNPRecord implements SNPRecord {
+		private String line;
+		private String name, chromosome;
+		private int position;
+		private double genTrainScore;
+		private String SNP;
+		private String ilmnStrand, customerStrand;
+		private String normID;
+
+		private ParsedSNPRecord(String line) {
+			if (line == null)
+				throw new NullPointerException("line");
+			this.line = line;
+
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			if (tokenizer.countTokens() != 9)
+				throw new IllegalArgumentException(line);
+			tokenizer.nextToken(); // Discard index field.
+			this.name = tokenizer.nextToken();
+			this.chromosome = tokenizer.nextToken();
+			this.position = Integer.parseInt(tokenizer.nextToken());
+			if (position < 1)
+				throw new IllegalArgumentException(String.valueOf(position));
+			this.genTrainScore = Double.parseDouble(tokenizer.nextToken());
+			this.SNP = tokenizer.nextToken();
+			this.ilmnStrand = tokenizer.nextToken();
+			this.customerStrand = tokenizer.nextToken();
+			this.normID = tokenizer.nextToken();
+		}
+
+		public String getName() { return name; }
+		public String getChromosome() { return chromosome; }
+		public int getPosition() { return position; }
+		public double getGenTrainScore() { return genTrainScore; }
+		public String getSNP() { return SNP; }
+		public String getILMNStrand() { return ilmnStrand; }
+		public String getCustomerStrand() { return customerStrand; }
+		public String getNormID() { return normID; }
+		public String toString() { return line; }
+	}
+}
