e408ae40722a0723be201c74b0689222a5de83ed
jelaiw <jelaiw@users.noreply.github.com>
2008-09-30 17:09:23 +0000

    Added first pass at javadoc comments for Sample and Population.

--- src/java/edu/uab/ssg/model/snp/Population.java
+++ src/java/edu/uab/ssg/model/snp/Population.java
@@ -30,14 +30,52 @@ public interface Population {
 	 */
 	Set<SNP> getSNPs();
 
+	/**
+	 * Returns allele info for a particular SNP.
+	 */
 	AlleleInfo getAlleleInfo(SNP snp);
 
+	/**
+	 * Information about the alleles at a particular SNP, including names, 
+	 * frequencies, and number of missing values.
+	 */
 	interface AlleleInfo {
+		/**
+		 * Returns the alleles.
+		 */
 		Set<String> getAlleles();
+
+		/**
+		 * Returns the frequency, or count, of a particular allele within
+		 * the study population.
+		 */
 		int getFrequency(String allele);
+
+		/**
+		 * Returns the relative frequency of a particular allele within
+		 * the study population.
+		 */
 		double getRelativeFrequency(String allele);
+
+		/**
+		 * Returns true if the minor allele exists.
+		 * The minor allele is the less frequent allele and is defined only if 
+		 * there is more than one allele at this SNP in the study population.
+		 */
 		boolean existsMinorAllele();
+
+		/**
+		 * Returns the minor allele.
+		 * If the minor allele does not exist, a runtime exception is thrown.
+		 */
 		String getMinorAllele();
+
+		/**
+		 * Returns the number of missing (allele) values.
+		 * A value is missing if a sample from the study population was
+		 * assessed for genotype at a particular SNP, but, for whatever reason,
+		 * the allele(s) could not be determined.
+		 */
 		int getNumberOfMissingValues();
 	}
 }
--- src/java/edu/uab/ssg/model/snp/Sample.java
+++ src/java/edu/uab/ssg/model/snp/Sample.java
@@ -1,19 +1,54 @@
 package edu.uab.ssg.model.snp;
 
 /**
+ * A sample from a study population.
+ *
  * @author Jelai Wang
  */
 
 public interface Sample {
+	/**
+	 * Returns the name of the sample.
+	 */
 	String getName();
+
+	/**
+	 * Returns the study population to which this sample belongs.
+	 */
 	Population getPopulation();
+
+	/**
+	 * Returns true if genotype for the given SNP was assessed in this sample.
+	 */
 	boolean existsGenotype(SNP snp);
+
+	/**
+	 * Returns the genotype of the sample for the given SNP.
+	 */
 	Genotype getGenotype(SNP snp);
 
+	/**
+	 * A genotype for a sample at a particular SNP.
+	 */
 	interface Genotype {
+		/**
+		 * Returns the SNP for this genotype.
+		 */
 		SNP getSNP();
+
+		/**
+		 * Returns the first allele of this genotype.
+		 */
 		String getAllele1();
+
+		/**
+		 * Returns the second allele of this genotype.
+		 */
 		String getAllele2();
+
+		/**
+		 * Returns the strand of the alleles of this genotype.
+		 */
 		Strand getStrand();
 	}
 }
