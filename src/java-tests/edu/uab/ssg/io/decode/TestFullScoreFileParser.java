718077d55461a4dcfe1d94227f400e4e5aba69d5
jelaiw <jelaiw@users.noreply.github.com>
2008-11-10 21:45:42 +0000

    Added first pass at parser, unit test, and test file for the snps_fullscore_20080328.tab file compiled by Gisli Masson at deCODE and circulated to the UAB popgen team on March 28, 2008.

---
+++ src/java-tests/edu/uab/ssg/io/decode/TestFullScoreFileParser.java
@@ -0,0 +1,98 @@
+package edu.uab.ssg.io.decode;
+
+import junit.framework.TestCase;
+import junit.framework.Assert;
+import java.io.*;
+
+/**
+ * @author Jelai Wang
+ */
+
+public final class TestFullScoreFileParser extends TestCase {
+	public void testExampleFile() throws IOException {
+		InputStream in = getClass().getClassLoader().getResourceAsStream("edu/uab/ssg/io/decode/fullscore.txt");
+		FullScoreFileParser parser = new FullScoreFileParser();
+		TestHelper helper = new TestHelper();
+		parser.parse(in, helper);
+		Assert.assertEquals(15, helper.getNumberOfParsedRecords());
+		Assert.assertEquals(0, helper.getNumberOfBadRecords());
+	}
+
+	private static final class TestHelper implements FullScoreFileParser.RecordListener {
+		private int numOfParsedRecords = 0;
+		private int numOfBadRecords = 0;
+
+		public void handleParsedRecord(FullScoreFileParser.SNPRecord record) {
+			// Spot check specific records.
+			if (numOfParsedRecords == 0) { // First record.
+				Assert.assertEquals("rs714630", record.getSNPName());
+				Assert.assertEquals("12", record.getChr());
+				Assert.assertEquals(8657652, record.getCoordinate());
+				Assert.assertTrue(record.isInclude());
+				Assert.assertFalse(record.isMHC());
+				Assert.assertFalse(record.isAIM());
+				Assert.assertFalse(record.isCNV());
+				Assert.assertFalse(record.isHarley());
+			}
+			else if (numOfParsedRecords == 10) { // MHC record.
+				Assert.assertEquals("rs592625", record.getSNPName());
+				Assert.assertEquals("6", record.getChr());
+				Assert.assertEquals(33080668, record.getCoordinate());
+				Assert.assertTrue(record.isInclude());
+				Assert.assertTrue(record.isMHC());
+				Assert.assertFalse(record.isAIM());
+				Assert.assertFalse(record.isCNV());
+				Assert.assertFalse(record.isHarley());
+			}
+			else if (numOfParsedRecords == 11) { // AIM record.
+				Assert.assertEquals("rs10313", record.getSNPName());
+				Assert.assertEquals("5", record.getChr());
+				Assert.assertEquals(180615011, record.getCoordinate());
+				Assert.assertFalse(record.isInclude());
+				Assert.assertFalse(record.isMHC());
+				Assert.assertTrue(record.isAIM());
+				Assert.assertFalse(record.isCNV());
+				Assert.assertFalse(record.isHarley());
+			}
+			else if (numOfParsedRecords == 12) { // CNV record.
+				Assert.assertEquals("CNV_cnv11927p2", record.getSNPName());
+				Assert.assertEquals("8", record.getChr());
+				Assert.assertEquals(6727787, record.getCoordinate());
+				Assert.assertFalse(record.isInclude());
+				Assert.assertFalse(record.isMHC());
+				Assert.assertFalse(record.isAIM());
+				Assert.assertTrue(record.isCNV());
+				Assert.assertFalse(record.isHarley());
+			}
+			else if (numOfParsedRecords == 13) { // Harley record.
+				Assert.assertEquals("rs2280789", record.getSNPName());
+				Assert.assertEquals("17", record.getChr());
+				Assert.assertEquals(31231116, record.getCoordinate());
+				Assert.assertTrue(record.isInclude());
+				Assert.assertFalse(record.isMHC());
+				Assert.assertFalse(record.isAIM());
+				Assert.assertFalse(record.isCNV());
+				Assert.assertTrue(record.isHarley());
+			}
+			else if (numOfParsedRecords == 14) { // Not included, Harley record.
+				Assert.assertEquals("rs10002897", record.getSNPName());
+				Assert.assertEquals("4", record.getChr());
+				Assert.assertEquals(74489288, record.getCoordinate());
+				Assert.assertFalse(record.isInclude());
+				Assert.assertFalse(record.isMHC());
+				Assert.assertFalse(record.isAIM());
+				Assert.assertFalse(record.isCNV());
+				Assert.assertTrue(record.isHarley());
+			}
+			// Increment total number of handled records.
+			numOfParsedRecords++;
+		}
+
+		public void handleBadRecordFormat(String line) {
+			numOfBadRecords++;
+		}
+
+		private int getNumberOfParsedRecords() { return numOfParsedRecords; }
+		private int getNumberOfBadRecords() { return numOfBadRecords; }
+	}
+}
---
+++ src/java/edu/uab/ssg/io/decode/FullScoreFileParser.java
@@ -0,0 +1,135 @@
+package edu.uab.ssg.io.decode;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * A parser for the <i>snps_fullscore_20080328.tab</i> file compiled by Gisli 
+ * Masson at deCODE and circulated to the UAB popgen team on March 28, 2008.
+ *
+ * @author Jelai Wang
+ */
+public final class FullScoreFileParser {
+	/**
+	 * Constructs the parser.
+	 */
+	public FullScoreFileParser() {
+	}
+
+	/**
+	 * Parses the input stream for SNP records.
+	 * @param in The input stream, typically a file input stream, of the
+	 * SNP file to be parsed.
+	 * @param listener As the input stream is parsed, each SNP record
+	 * is passed to the user-supplied record listener.
+	 */
+	public void parse(InputStream in, RecordListener listener) throws IOException {
+		if (in == null)
+			throw new NullPointerException("in");
+		if (listener == null)
+			throw new NullPointerException("listener");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		// Process header.
+		String[] header = reader.readLine().split("\t"); 
+		if (header.length != 90)
+			throw new IllegalArgumentException(String.valueOf(header.length));
+		if (!"SNP_Name".equals(header[0]))
+			throw new IllegalArgumentException(header[0]);
+		if (!"Chr".equals(header[3]))
+			throw new IllegalArgumentException(header[3]);
+		if (!"Coordinate".equals(header[4]))
+			throw new IllegalArgumentException(header[4]);
+		if (!"INCLUDE".equals(header[49]))
+			throw new IllegalArgumentException(header[49]);
+		if (!"MHC".equals(header[50]))
+			throw new IllegalArgumentException(header[50]);
+		if (!"AIM".equals(header[51]))
+			throw new IllegalArgumentException(header[51]);
+		if (!"CNV".equals(header[52]))
+			throw new IllegalArgumentException(header[52]);
+		if (!"HARLEY".equals(header[53]))
+			throw new IllegalArgumentException(header[53]);
+		// Process other rows.
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			SNPRecord record = null;
+			try {
+				record = new ParsedSNPRecord(line);
+			}
+			catch (RuntimeException e) {
+				listener.handleBadRecordFormat(line);
+				continue;
+			}
+			listener.handleParsedRecord(record);
+		}
+		reader.close();
+	}
+
+	/**
+	 * A listener for handling parsed SNP records and problems due to
+	 * bad record formatting.
+	 */
+	public interface RecordListener {
+		/**
+		 * Handles successfully parsed SNP record.
+		 */
+		void handleParsedRecord(SNPRecord record);
+
+		/**
+		 * Handles input that could not be parsed due to a formatting problem.
+		 * @param line The line of text that could not be parsed.
+		 */
+		void handleBadRecordFormat(String line);
+	}
+
+	/**
+	 * A SNP record.
+	 */
+	public interface SNPRecord {
+		String getSNPName();
+		String getChr();
+		int getCoordinate();
+		boolean isInclude();
+		boolean isMHC();
+		boolean isAIM();
+		boolean isCNV();
+		boolean isHarley();
+	}
+
+	private class ParsedSNPRecord implements SNPRecord {
+		private String line;
+		private String name, chr;
+		private int coordinate;
+		private boolean include, mhc, aim, cnv, harley;
+
+		private ParsedSNPRecord(String line) {
+			if (line == null)
+				throw new NullPointerException("line");
+			this.line = line;
+
+			String[] tmp = line.split("\t");
+			if (tmp.length != 90) // LOOK!! We expect 90 fields per record.
+				throw new IllegalArgumentException(line);
+			this.name = tmp[0];
+			this.chr = tmp[3];
+			this.coordinate = Integer.parseInt(tmp[4]);
+			if (coordinate < 1)
+				throw new IllegalArgumentException(String.valueOf(coordinate));
+			this.include = "1".equals(tmp[49]);
+			this.mhc = "1".equals(tmp[50]);
+			this.aim = "1".equals(tmp[51]);
+			this.cnv = "1".equals(tmp[52]);
+			this.harley = "1".equals(tmp[53]);
+		}
+
+		public String getSNPName() { return name; }
+		public String getChr() { return chr; }
+		public int getCoordinate() { return coordinate; }
+		public boolean isInclude() { return include; }
+		public boolean isMHC() { return mhc; }
+		public boolean isAIM() { return aim; }
+		public boolean isCNV() { return cnv; }
+		public boolean isHarley() { return harley; }
+		public String toString() { return line; }
+	}
+}
