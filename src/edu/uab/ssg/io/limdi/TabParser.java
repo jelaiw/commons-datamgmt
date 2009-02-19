8b1bab118b00ce6fe925843d2a061e77376cbc27
jelaiw <jelaiw@users.noreply.github.com>
2009-02-19 19:16:11 +0000

    Added first pass at parser and unit test for the tab-delimited text file (GWAS_AA_feb19_09_B109.tab) containing the de-identified and coded clinical data for the Limdi warfarin analysis.

---
+++ src/edu/uab/ssg/io/limdi/TabParser.java
@@ -0,0 +1,128 @@
+package edu.uab.ssg.io.limdi;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * A parser for the tab-delimited text file, called <i>GWAS_AA_feb19_09_B109.tab</i>, containing the de-identified and coded clinical data from the Limdi warfarin analysis.
+ *
+ * <p><tt>
+ * IDnum<br/>
+ * Gender<br/>
+ * AvgDose<br/>
+ * Stabledose<br/>
+ * </tt></p>
+ *
+ * This file was created by opening the original MS Excel spreadsheet
+ * and saving as tab-delimited text in MS Excel 2000.
+ *
+ * @author Jelai Wang
+ */
+public final class TabParser {
+	/**
+	 * Constructs the parser.
+	 */
+	public TabParser() {
+	}
+
+	/**
+	 * A listener for handling parsed sample records and problems due to
+	 * bad record formatting.
+	 */
+	public interface RecordListener {
+		/**
+		 * Handles successfully parsed sample record.
+		 */
+		void handleParsedRecord(SampleRecord record);
+
+		/**
+		 * Handles input that could not be parsed due to a formatting problem.
+		 * @param line The line of text that could not be parsed.
+		 */
+		void handleBadRecordFormat(String line);
+	}
+
+	/**
+	 * An sample record.
+	 */
+	public interface SampleRecord {
+		String getIDNum();
+		String getGender();
+		String getAvgDose();
+		String getStabledose();
+	}
+
+	/**
+	 * Parses the input stream for sample records.
+	 * @param in The input stream, typically a file input stream, of the
+	 * file to be parsed.
+	 * @param listener As the input stream is parsed, each sample record
+	 * is passed to the user-supplied record listener.
+	 */
+	public void parse(InputStream in, RecordListener listener) throws IOException {
+		if (in == null)
+			throw new NullPointerException("in");
+		if (listener == null)
+			throw new NullPointerException("listener");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		String header = reader.readLine();
+		String[] tmp = header.split("\t");
+		// Spot check a few expected column names in the header.
+		if (!"IDNum".equals(dequote(tmp[0])))
+			throw new IllegalArgumentException(header);
+		if (!"Gender".equals(dequote(tmp[1])))
+			throw new IllegalArgumentException(header);
+		if (!"AvgDose".equals(dequote(tmp[2])))
+			throw new IllegalArgumentException(header);
+		if (!"Stabledose".equals(dequote(tmp[3])))
+			throw new IllegalArgumentException(header);
+		// Process the rest of the rows.
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			SampleRecord record = null;
+			try {
+				record = new ParsedSampleRecord(line);
+			}
+			catch (RuntimeException e) {
+				listener.handleBadRecordFormat(line);
+				continue;
+			}
+			listener.handleParsedRecord(record);
+		}
+		reader.close();
+	}
+
+	private String dequote(String tmp) {
+		if (tmp.startsWith("\"") && tmp.endsWith("\""))
+			return tmp.substring(1, tmp.length() - 1);
+		return tmp;	
+	}
+
+	private class ParsedSampleRecord implements SampleRecord {
+		private String line;
+		private String idnum, gender, avgdose, stabledose;
+
+		private ParsedSampleRecord(String line) {
+			if (line == null)
+				throw new NullPointerException("line");
+			this.line = line;
+
+			String[] tmp = line.split("\t", -1); // See String API.
+
+			if (tmp.length != 4) // Expect 4 columns.
+				throw new IllegalArgumentException(tmp.length + "," + line);
+			this.idnum = dequote(tmp[0]);
+			this.gender = dequote(tmp[1]);
+			this.avgdose = dequote(tmp[2]);
+			this.stabledose = dequote(tmp[3]);
+			if ("".equals(stabledose)) stabledose = null; // LOOK!!
+		}
+
+		public String getIDNum() { return idnum; }
+		public String getGender() { return gender; }
+		public String getAvgDose() { return avgdose; }
+		public String getStabledose() { return stabledose; }
+
+		public String toString() { return line; }
+	}
+}
---
+++ test/edu/uab/ssg/io/limdi/TestTabParser.java
@@ -0,0 +1,56 @@
+package edu.uab.ssg.io.limdi;
+
+import junit.framework.TestCase;
+import junit.framework.Assert;
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author Jelai Wang
+ */
+public final class TestTabParser extends TestCase {
+	public void testExampleFile() throws IOException {
+		InputStream in = getClass().getClassLoader().getResourceAsStream("edu/uab/ssg/io/limdi/test.tab");
+		TabParser parser = new TabParser();
+		TestHelper helper = new TestHelper();
+		parser.parse(in, helper);
+		Assert.assertEquals(6, helper.getNumberOfParsedRecords());
+		Assert.assertEquals(0, helper.getNumberOfBadRecords());
+	}
+
+	private static final class TestHelper implements TabParser.RecordListener {
+		private int numOfParsedRecords = 0;
+		private int numOfBadRecords = 0;
+
+		public void handleParsedRecord(TabParser.SampleRecord record) {
+			// Spot check specific records.
+			if (numOfParsedRecords == 0) { // First record in file.
+				Assert.assertEquals("A0001", record.getIDNum());
+				Assert.assertEquals("M", record.getGender());
+				Assert.assertEquals("5.048701308", record.getAvgDose());
+				Assert.assertNull(record.getStabledose());
+			}
+			else if (numOfParsedRecords == 1) {
+				Assert.assertEquals("A0003", record.getIDNum());
+				Assert.assertEquals("M", record.getGender());
+				Assert.assertEquals("6.061213134", record.getAvgDose());
+				Assert.assertEquals("5.714285851", record.getStabledose());
+			}
+			else if (numOfParsedRecords == 5) { // Last record in file.
+				Assert.assertEquals("B0109", record.getIDNum());
+				Assert.assertEquals("M", record.getGender());
+				Assert.assertEquals("4.563492094", record.getAvgDose());
+				Assert.assertNull(record.getStabledose());
+			}
+			// Increment total number of handled records.
+			numOfParsedRecords++;
+		}
+
+		public void handleBadRecordFormat(String line) {
+			numOfBadRecords++;
+		}
+
+		private int getNumberOfParsedRecords() { return numOfParsedRecords; }
+		private int getNumberOfBadRecords() { return numOfBadRecords; }
+	}
+}
