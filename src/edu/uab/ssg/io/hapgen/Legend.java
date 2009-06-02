28be89c10df7f2faf78cbfb2a35dccd846b4f7d3
jelaiw <jelaiw@users.noreply.github.com>
2009-06-02 21:20:39 +0000

    Added first pass at javadoc comments.

--- src/edu/uab/ssg/io/hapgen/Legend.java
+++ src/edu/uab/ssg/io/hapgen/Legend.java
@@ -5,11 +5,37 @@ import java.io.*;
 import java.util.*;
 
 /**
+ * An abstraction for the "legend" concept and file format required by the HapGen software at <a href="http://www.stats.ox.ac.uk/~marchini/software/gwas/hapgen.html">http://www.stats.ox.ac.uk/~marchini/software/gwas/hapgen.html</a>.
+ * An example of the file format, taken from the <i>ex.leg</i> file in HapGen version 1.3.0, is below:
+ *
+ * <p><tt>
+ * rs      position        0       1<br/>
+ * rs11089130      14431347        C       G<br/>
+ * rs738829        14432618        A       G<br/>
+ * rs915674        14433624        A       G<br/>
+ * rs915675        14433659        A       C<br/>
+ * rs915677        14433758        A       G<br/>
+ * rs9604721       14434713        C       T<br/>
+ * rs4389403       14435207        A       G<br/>
+ * rs5746356       14439734        C       T<br/>
+ * rs9617528       14441016        C       T<br/>
+ * rs2154787       14449374        C       T<br/>
+ * </tt></p>
+ *
+ * The field delimiter is the space character. Missing data is coded as the hyphen (dash) character. See the <a href="http://www.stats.ox.ac.uk/~marchini/software/gwas/hapgen.html#Options">HapGen Options</a> section on the -l switch for more detail, including a link to example legend files from HapMap at <a href="http://www.hapmap.org/downloads/phasing/2006-07_phaseII/all/">http://www.hapmap.org/downloads/phasing/2006-07_phaseII/all/</a>. Also see <a href="http://www.hapmap.org/downloads/phasing/2006-07_phaseII/00README.txt">this README</a>.
+ *
  * @author Jelai Wang
  */
 public final class Legend {
 	private Map<SNP, AlleleCounter> snp2counter = new LinkedHashMap<SNP, AlleleCounter>();
 
+	/**
+	 * Constructs the legend.
+	 * @param snps The given samples will be queried at the SNPs provided in
+	 * this list to establish how the alleles (usually in nucleotide bases)
+	 * are recoded as allele 0 or 1.
+	 * @param samples The set of samples of interest in the study population.
+	 */
 	public Legend(List<SNP> snps, Set<Sample> samples) {
 		if (snps == null)
 			throw new NullPointerException("snps");
@@ -41,8 +67,14 @@ public final class Legend {
 		}
 	}
 
+	/**
+	 * Returns the SNPs for which this legend provides allele 0 and 1 recodes.
+	 */
 	public List<SNP> getSNPs() { return new ArrayList<SNP>(snp2counter.keySet()); }
 
+	/**
+	 * Returns the allele coded as allele 0 at this SNP.
+	 */
 	public String getAllele0(SNP snp) {
 		if (snp == null)
 			throw new NullPointerException("snp");
@@ -63,6 +95,9 @@ public final class Legend {
 		return null;
 	}
 
+	/**
+	 * Returns the allele coded as allele 1 at this SNP.
+	 */
 	public String getAllele1(SNP snp) {
 		if (snp == null)
 			throw new NullPointerException("snp");
@@ -78,6 +113,9 @@ public final class Legend {
 		return null;
 	}
 
+	/**
+	 * Writes this legend to the given output stream.
+	 */
 	public void write(OutputStream out) throws IOException {
 		if (out == null)
 			throw new NullPointerException("out");
@@ -87,6 +125,9 @@ public final class Legend {
 		writer.close();
 	}
 
+	/**
+	 * Returns a tabular string representation of this legend.
+	 */
 	public String toString() {
 		char DELIMITER = ' ';
 	   	String EOL = "\n";
