b11aee66d8655272b1af1ca102f371d835a359f5
jelaiw <jelaiw@users.noreply.github.com>
2009-06-04 15:58:09 +0000

    Added first pass at javadoc comments.

--- src/edu/uab/ssg/io/hapgen/LegendBuilder.java
+++ src/edu/uab/ssg/io/hapgen/LegendBuilder.java
@@ -4,6 +4,8 @@ import edu.uab.ssg.model.snp.*;
 import java.util.*;
 
 /**
+ * A builder for Legend implementations.
+ *
  * @author Jelai Wang
  */
 
@@ -11,9 +13,17 @@ public final class LegendBuilder {
 	private Map<SNP, AlleleCounter> snp2counter = new LinkedHashMap<SNP, AlleleCounter>();
 	private Map<SNP, AlleleCounter> badsnps = new LinkedHashMap<SNP, AlleleCounter>();
 
+	/**
+	 * Constructs the builder.
+	 */
 	public LegendBuilder() {
 	}
 
+	/**
+	 * Counts an allele at a SNP.
+	 * The set of alleles observed at a SNP determines how the alleles
+	 * are recoded to 0 or 1 in the legend.
+	 */
 	public void countAllele(SNP snp, String allele) {
 		if (snp == null)
 			throw new NullPointerException("snp");
@@ -39,16 +49,30 @@ public final class LegendBuilder {
 		}
 	}
 
+	/**
+	 * Creates legend that recodes alleles to 0 or 1 based on the minor allele.
+	 */
 	public Legend createMinorAlleleLegend() {
 		return new MinorAlleleLegend(snp2counter);
 	}
 
+	/**
+	 * Creates legend that recodes alleles to 0 or 1 based on the haplotype of a given reference sample.
+	 */
 	public Legend createReferenceSampleLegend(Sample referenceSample) {
 		return new ReferenceSampleLegend(referenceSample, snp2counter);
 	}
 
+	/**
+	 * Returns a list of SNPs that are not monomorphic or biallelic.
+	 * These SNPs are not part of the legend because the recoding of alleles
+	 * to 0 or 1 is only defined for monomorphic or biallelic SNPs.
+	 */
 	public List<SNP> getBadSNPs() { return new ArrayList<SNP>(badsnps.keySet()); }
 
+	/**
+	 * For a SNP that is not monomorphic or biallelic, return the set of observed alleles for troubleshooting purposes.
+	 */
 	public Set<String> getAllelesForBadSNP(SNP badsnp) {
 		if (badsnp == null)
 			throw new NullPointerException("badsnp");
