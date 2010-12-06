ea78e630efaf2730c3622b1ce5b1d5688ea60335
jelaiw <jelaiw@users.noreply.github.com>
2010-12-06 18:03:02 +0000

    Added first pass at parser, unit test, and test file (first 100 lines from Molly Bray ImmunoChip genotyping report for Dick Kaslow) for more flexibly parsing the various formats that the Illumina genotyping report can have.

---
+++ src/edu/uab/ssg/io/illumina/GenotypeFileParser.java
@@ -0,0 +1,134 @@
+package edu.uab.ssg.io.illumina;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * A parser that can flexibly handle the various formats that Illumina genotyping reports can have. 
+ *
+ * Here is an example:
+ * <p><tt>
+ * [Header]<br/>
+ * GSGT Version	1.7.4<br/>
+ * Processing Date	10/29/2010 11:42<br/>
+ * Content		Immuno_BeadChip_11419691_B.bpm<br/>
+ * Num SNPs	196492<br/>
+ * Total SNPs	196524<br/>
+ * Num Samples	768<br/>
+ * Total Samples	768<br/>
+ * [Data]<br/>
+ * SNP Name	Sample ID	Allele1 - Top	Allele2 - Top	Allele1 - AB	Allele2 - AB	Chr	Position<br/>
+ * 1-159076491-G-DELETION	AVA1001a	I	I	B	B	1	159076491<br/>
+ * 1-159093319-A-DELETION	AVA1001a	I	I	A	A	1	159093319<br/>
+ * 1-159105568-A-INSERTION	AVA1001a	I	I	A	A	1	159105568<br/>
+ * ...
+ * </tt></p>
+ *
+ * <p>In this implementation, the data table record delimiter is the tab character.</p>
+ *
+ * @author Jelai Wang
+ */
+public final class GenotypeFileParser {
+	private static final String MISSING_ALLELE = "-";
+
+	/**
+	 * Constructs the parser.
+	 */
+	public GenotypeFileParser() {
+	}
+
+	/**
+	 * Parses the input stream for genotype records.
+	 * @param in The input stream, typically a file input stream, of the
+	 * file to be parsed.
+	 * @param listener As the input stream is parsed, each genotype record
+	 * is passed to the user-supplied record listener.
+	 */
+	public void parse(InputStream in, RecordListener listener) throws IOException {
+		if (in == null)
+			throw new NullPointerException("in");
+		if (listener == null)
+			throw new NullPointerException("listener");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		String DELIMITER = "\t";
+		List<String> header = null;
+		String line = null;
+		boolean inDataSection = false;
+		while ((line = reader.readLine()) != null) {
+			if (!inDataSection && "[Data]".equals(line)) { // Detect the beginning of the data section.
+				inDataSection = true;
+				header = Arrays.asList(reader.readLine().split(DELIMITER, -1));
+			}
+			else if (inDataSection) {
+				String[] tokens = line.split(DELIMITER, -1);
+				if (tokens.length != header.size()) {
+					listener.handleBadRecordFormat(line);
+					continue;
+				}
+				GenotypeRecord record = new ParsedGenotypeRecord(line, header, tokens);
+				listener.handleParsedRecord(record);
+			}
+			// Skip all other lines, usually just the header section.
+		}
+		reader.close();
+	}
+
+	/**
+	 * A listener for handling parsed genotype records and problems due to
+	 * bad record formatting.
+	 */
+	public interface RecordListener {
+		/**
+		 * Handles successfully parsed genotype record.
+		 */
+		void handleParsedRecord(GenotypeRecord record);
+
+		/**
+		 * Handles input that could not be parsed due to a formatting problem.
+		 * @param line The line of text that could not be parsed.
+		 */
+		void handleBadRecordFormat(String line);
+	}
+
+	/**
+	 * A genotype record.
+	 */
+	public interface GenotypeRecord {
+		String getValue(int columnIndex);
+		String getValue(String columnName);
+		List<String> getColumnNames();
+	}
+
+	private class ParsedGenotypeRecord implements GenotypeRecord {
+		private String line;
+		private List<String> header;
+		private String[] tokens;
+
+		private ParsedGenotypeRecord(String line, List<String> header, String[] tokens) {
+			if (line == null)
+				throw new NullPointerException("line");
+			if (header == null)
+				throw new NullPointerException("header");
+			if (tokens == null)
+				throw new NullPointerException("tokens");
+			this.line = line;
+			this.header = header;
+			this.tokens = tokens;
+		}
+
+		public String getValue(int columnIndex) {
+			return tokens[columnIndex];
+		}
+
+		public String getValue(String columnName) {
+			if (columnName == null)
+				throw new NullPointerException("columnName");
+			if (!header.contains(columnName))
+				throw new IllegalArgumentException(columnName);
+			return getValue(header.indexOf(columnName));
+		}
+
+		public List<String> getColumnNames() { return new ArrayList<String>(header); }
+		public String toString() { return line; }
+	}
+}
---
+++ test/edu/uab/ssg/io/illumina/TestGenotypeFileParser.java
@@ -0,0 +1,51 @@
+package edu.uab.ssg.io.limdi;
+
+import junit.framework.TestCase;
+import junit.framework.Assert;
+import java.io.*;
+
+/**
+ * @author Jelai Wang
+ */
+
+public final class TestGenotypeFileParser extends TestCase {
+	public void testExampleFile() throws IOException {
+		// This input file contains representative records from the Illumina genotype report in the Limdi warfarin study as well as purposefully spiked-in file format errors.
+		InputStream in = getClass().getClassLoader().getResourceAsStream("edu/uab/ssg/io/limdi/genotypes.txt");
+		GenotypeFileParser parser = new GenotypeFileParser();
+		TestHelper helper = new TestHelper();
+		parser.parse(in, helper);
+		Assert.assertEquals(12, helper.getNumberOfParsedRecords());
+		Assert.assertEquals(2, helper.getNumberOfBadRecords());
+	}
+
+	private static final class TestHelper implements GenotypeFileParser.RecordListener {
+		private int numOfParsedRecords = 0;
+		private int numOfBadRecords = 0;
+
+		public void handleParsedRecord(GenotypeFileParser.GenotypeRecord record) {
+			// Spot check specific records.
+			if (numOfParsedRecords == 0) { // First record in file.
+				Assert.assertEquals("200003", record.getSNPName());
+				Assert.assertEquals("A0001", record.getSampleID());
+				Assert.assertEquals("G", record.getAllele1Forward());
+				Assert.assertEquals("G", record.getAllele2Forward());
+			}
+			else if (numOfParsedRecords == 11) { // Last record in file.
+				Assert.assertEquals("cnvi0048855", record.getSNPName());
+				Assert.assertEquals("A0280", record.getSampleID());
+				Assert.assertEquals(null, record.getAllele1Forward());
+				Assert.assertEquals(null, record.getAllele2Forward());
+			}
+			// Increment total number of handled records.
+			numOfParsedRecords++;
+		}
+
+		public void handleBadRecordFormat(String line) {
+			numOfBadRecords++;
+		}
+
+		private int getNumberOfParsedRecords() { return numOfParsedRecords; }
+		private int getNumberOfBadRecords() { return numOfBadRecords; }
+	}
+}
