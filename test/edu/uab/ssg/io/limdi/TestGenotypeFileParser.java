60770097a082e4704d56d6e611757cce0850f528
jelaiw <jelaiw@users.noreply.github.com>
2010-12-07 16:44:35 +0000

    Fixed a curious transposition in the proper location of these files that manages to still do the right thing when executing the unit tests. Still needed to be fixed to avoid future confusion.

--- test/edu/uab/ssg/io/limdi/TestGenotypeFileParser.java
+++ test/edu/uab/ssg/io/limdi/TestGenotypeFileParser.java
@@ -1,4 +1,4 @@
-package edu.uab.ssg.io.illumina;
+package edu.uab.ssg.io.limdi;
 
 import junit.framework.TestCase;
 import junit.framework.Assert;
@@ -10,13 +10,13 @@ import java.io.*;
 
 public final class TestGenotypeFileParser extends TestCase {
 	public void testExampleFile() throws IOException {
-		// This input file contains representative records from the Illumina genotype report for an ImmunoChip experiment from Molly Bray's lab.
-		InputStream in = getClass().getClassLoader().getResourceAsStream("edu/uab/ssg/io/illumina/test100.txt");
+		// This input file contains representative records from the Illumina genotype report in the Limdi warfarin study as well as purposefully spiked-in file format errors.
+		InputStream in = getClass().getClassLoader().getResourceAsStream("edu/uab/ssg/io/limdi/genotypes.txt");
 		GenotypeFileParser parser = new GenotypeFileParser();
 		TestHelper helper = new TestHelper();
 		parser.parse(in, helper);
-		Assert.assertEquals(90, helper.getNumberOfParsedRecords());
-		Assert.assertEquals(0, helper.getNumberOfBadRecords());
+		Assert.assertEquals(12, helper.getNumberOfParsedRecords());
+		Assert.assertEquals(2, helper.getNumberOfBadRecords());
 	}
 
 	private static final class TestHelper implements GenotypeFileParser.RecordListener {
@@ -26,24 +26,16 @@ public final class TestGenotypeFileParser extends TestCase {
 		public void handleParsedRecord(GenotypeFileParser.GenotypeRecord record) {
 			// Spot check specific records.
 			if (numOfParsedRecords == 0) { // First record in file.
-				Assert.assertEquals("1-159076491-G-DELETION", record.getValue("SNP Name"));
-				Assert.assertEquals("AVA1001a", record.getValue("Sample ID"));
-				Assert.assertEquals("I", record.getValue("Allele1 - Top"));
-				Assert.assertEquals("I", record.getValue("Allele2 - Top"));
-				Assert.assertEquals("B", record.getValue("Allele1 - AB"));
-				Assert.assertEquals("B", record.getValue("Allele2 - AB"));
-				Assert.assertEquals("1", record.getValue("Chr"));
-				Assert.assertEquals("159076491", record.getValue("Position"));
+				Assert.assertEquals("200003", record.getSNPName());
+				Assert.assertEquals("A0001", record.getSampleID());
+				Assert.assertEquals("G", record.getAllele1Forward());
+				Assert.assertEquals("G", record.getAllele2Forward());
 			}
-			else if (numOfParsedRecords == 90) { // Last record in file.
-				Assert.assertEquals("1_211076006", record.getValue("SNP Name"));
-				Assert.assertEquals("AVA1001a", record.getValue("Sample ID"));
-				Assert.assertEquals("G", record.getValue("Allele1 - Top"));
-				Assert.assertEquals("G", record.getValue("Allele2 - Top"));
-				Assert.assertEquals("B", record.getValue("Allele1 - AB"));
-				Assert.assertEquals("B", record.getValue("Allele2 - AB"));
-				Assert.assertEquals("1", record.getValue("Chr"));
-				Assert.assertEquals("211076006", record.getValue("Position"));
+			else if (numOfParsedRecords == 11) { // Last record in file.
+				Assert.assertEquals("cnvi0048855", record.getSNPName());
+				Assert.assertEquals("A0280", record.getSampleID());
+				Assert.assertEquals(null, record.getAllele1Forward());
+				Assert.assertEquals(null, record.getAllele2Forward());
 			}
 			// Increment total number of handled records.
 			numOfParsedRecords++;
