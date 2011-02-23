a21ae5fb63571a52d8893825af0e9148e3ed354a
jelaiw <jelaiw@users.noreply.github.com>
2011-02-23 21:47:54 +0000

    Refactoring of the TestHelper inner class in preparation for some re-use by another test case. Added code comments explaining how the test.vcf test file was put together.

--- test/edu/uab/ssg/io/the1000genomes/TestVCFParser.java
+++ test/edu/uab/ssg/io/the1000genomes/TestVCFParser.java
@@ -9,44 +9,48 @@ import java.io.*;
  */
 
 public final class TestVCFParser extends TestCase {
-	public void testExampleFile() throws IOException {
+	// The test file is a cut down version of the VCF downloaded from ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20100804/ALL.2of4intersection.20100804.genotypes.vcf.gz with tabix for just chromosome 10.
+	public void testRealFile() throws IOException {
 		InputStream in = getClass().getClassLoader().getResourceAsStream("edu/uab/ssg/io/the1000genomes/test.vcf");
 		VCFParser parser = new VCFParser();
-		TestHelper helper = new TestHelper();
+		TestHelper helper = new TestHelper() {
+			public void handleParsedRecord(VCFParser.VariantRecord record) {
+				// Spot check specific records.
+				if (numOfParsedRecords == 0) { // First record.
+					Assert.assertEquals("10", record.getChromosome());
+					Assert.assertEquals(60523, record.getPosition());
+					Assert.assertNull(record.getID());
+					Assert.assertEquals("T", record.getReferenceAllele());
+					Assert.assertEquals("G", record.getAlternateAllele());
+				}
+				else if (numOfParsedRecords == 1) {
+					Assert.assertEquals("10", record.getChromosome());
+					Assert.assertEquals(61020, record.getPosition());
+					Assert.assertEquals("rs115033199", record.getID());
+					Assert.assertEquals("G", record.getReferenceAllele());
+					Assert.assertEquals("C", record.getAlternateAllele());
+				}
+				else if (numOfParsedRecords == 10) {
+					Assert.assertEquals("10", record.getChromosome());
+					Assert.assertEquals(135524737, record.getPosition());
+					Assert.assertNull(record.getID());
+					Assert.assertEquals("T", record.getReferenceAllele());
+					Assert.assertEquals("G", record.getAlternateAllele());
+				}
+				super.handleParsedRecord(record); // Should increment counter.
+			}
+		};
 		parser.parse(in, helper);
 		Assert.assertEquals(11, helper.getNumberOfParsedRecords());
 		Assert.assertEquals(0, helper.getNumberOfBadRecords());
 	}
 
-	private static final class TestHelper implements VCFParser.RecordListener {
-		private int numOfParsedRecords = 0;
-		private int numOfBadRecords = 0;
+	private static class TestHelper implements VCFParser.RecordListener {
+		protected int numOfParsedRecords = 0;
+		protected int numOfBadRecords = 0;
 
 		public void handleParsedRecord(VCFParser.VariantRecord record) {
-			// Spot check specific records.
-			if (numOfParsedRecords == 0) { // First record.
-				Assert.assertEquals("10", record.getChromosome());
-				Assert.assertEquals(60523, record.getPosition());
-				Assert.assertNull(record.getID());
-				Assert.assertEquals("T", record.getReferenceAllele());
-				Assert.assertEquals("G", record.getAlternateAllele());
-			}
-			else if (numOfParsedRecords == 1) {
-				Assert.assertEquals("10", record.getChromosome());
-				Assert.assertEquals(61020, record.getPosition());
-				Assert.assertEquals("rs115033199", record.getID());
-				Assert.assertEquals("G", record.getReferenceAllele());
-				Assert.assertEquals("C", record.getAlternateAllele());
-			}
-			else if (numOfParsedRecords == 10) {
-				Assert.assertEquals("10", record.getChromosome());
-				Assert.assertEquals(135524737, record.getPosition());
-				Assert.assertNull(record.getID());
-				Assert.assertEquals("T", record.getReferenceAllele());
-				Assert.assertEquals("G", record.getAlternateAllele());
-			}
-			// Increment total number of handled records.
-			numOfParsedRecords++;
+			numOfParsedRecords++; 
 		}
 
 		public void handleBadRecordFormat(String line) {
