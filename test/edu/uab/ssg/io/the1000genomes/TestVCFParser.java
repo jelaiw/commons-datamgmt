14236a4d5865652cbfb15846064eb2ebddf3e3ab
jelaiw <jelaiw@users.noreply.github.com>
2011-02-23 22:35:57 +0000

    Added a test case for the example file from the VCF specification at http://www.1000genomes.org/wiki/Analysis/Variant%20Call%20Format/vcf-variant-call-format-version-40.

--- test/edu/uab/ssg/io/the1000genomes/TestVCFParser.java
+++ test/edu/uab/ssg/io/the1000genomes/TestVCFParser.java
@@ -45,6 +45,35 @@ public final class TestVCFParser extends TestCase {
 		Assert.assertEquals(0, helper.getNumberOfBadRecords());
 	}
 
+	// The example is cut and pasted (with some squeezing of spaces in tr and replacement of spaces with tabs in vim) from the VCF specification at http://www.1000genomes.org/wiki/Analysis/Variant%20Call%20Format/vcf-variant-call-format-version-40. 
+	public void testExampleFile() throws IOException {
+		InputStream in = getClass().getClassLoader().getResourceAsStream("edu/uab/ssg/io/the1000genomes/example.vcf");
+		VCFParser parser = new VCFParser();
+		TestHelper helper = new TestHelper() {
+			public void handleParsedRecord(VCFParser.VariantRecord record) {
+				// Spot check specific records.
+				if (numOfParsedRecords == 0) { // In dbSNP.
+					Assert.assertEquals("20", record.getChromosome());
+					Assert.assertEquals(14370, record.getPosition());
+					Assert.assertEquals("rs6054257", record.getID());
+					Assert.assertEquals("G", record.getReferenceAllele());
+					Assert.assertEquals("A", record.getAlternateAllele());
+				}
+				else if (numOfParsedRecords == 1) { // Not in dbSNP.
+					Assert.assertEquals("20", record.getChromosome());
+					Assert.assertEquals(17330, record.getPosition());
+					Assert.assertNull(record.getID());
+					Assert.assertEquals("T", record.getReferenceAllele());
+					Assert.assertEquals("A", record.getAlternateAllele());
+				}
+				super.handleParsedRecord(record); // Should increment counter.
+			}
+		};
+		parser.parse(in, helper);
+		Assert.assertEquals(5, helper.getNumberOfParsedRecords());
+		Assert.assertEquals(0, helper.getNumberOfBadRecords());
+	}
+
 	private static class TestHelper implements VCFParser.RecordListener {
 		protected int numOfParsedRecords = 0;
 		protected int numOfBadRecords = 0;
